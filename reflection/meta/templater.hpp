bd27173eb5b4a2494b7483bf4e9830dd56e68c4a
Andrey Likhachev <crastin@yandex.ru>
2017-05-08 09:55:00 +0300

    Common fwd decl

--- reflection/meta/templater.hpp
+++ reflection/meta/templater.hpp
@@ -5,10 +5,16 @@ namespace templater {
 struct writing_visitor;
 struct reading_visitor;
 
+//template <typename Class>
+//void serialize_object(writing_visitor* visitor, Class const& obj) noexcept;
+//template <typename Class>
+//void deserialize_object(reading_visitor* visitor, Class& result) noexcept;
 template <typename Class>
-void serialize_object(writing_visitor* visitor, Class const& obj) noexcept;
+void serialize(writing_visitor* visitor, Class const& obj) noexcept;
+template <typename Class>
+void deserialize(reading_visitor* visitor, Class& result) noexcept;
 
-template <typename Class>
-void deserialize_object(reading_visitor* visitor, Class& result) noexcept;
+
+
 }
 
